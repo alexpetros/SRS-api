4bc2b149d5e65e3fcaf55a1e072dbac32caae49d
Alexander Petros <apetros15@gmail.com>
2018-11-15 14:49:39 -0500

    server model schema

---
+++ src/server.js
@@ -0,0 +1,47 @@
+// starter from http://cs52.me/assignments/sa/server-side/
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import path from 'path'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+
+// initialize
+const app = express()
+
+// enable/disable cross origin resource sharing if necessary
+app.use(cors())
+
+// enable/disable http request logging
+app.use(morgan('dev'))
+
+// enable only if you want templating
+app.set('view engine', 'ejs')
+
+// enable only if you want static assets from folder static
+app.use(express.static('static'))
+
+// this just allows us to render ejs from the ../app/views directory
+app.set('views', path.join(__dirname, '../src/views'))
+
+// enable json message body for posting data to API
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/cs52poll'
+mongoose.connect(mongoURI)
+mongoose.Promise = global.Promise
+
+
+// default index route
+app.get('/', (req, res) => {
+  res.send('hi')
+})
+
+// START THE SERVER
+// =============================================================================
+const port = process.env.PORT || 9090
+app.listen(port)
+
+console.log(`listening on: ${port}`)
